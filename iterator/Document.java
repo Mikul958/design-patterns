f990f3d9a8b47b5e70ffbd4db94e853819d5e9f1
Mikul958 <michaelpikula958@gmail.com>
2023-10-05 17:49:45 -0400

    Documented code.

--- iterator/Document.java
+++ iterator/Document.java
@@ -2,12 +2,19 @@ package iterator;
 
 import java.util.Stack;
 
+/**
+ * A class representing an editable document that contains an undo stack and redo stack.
+ */
 public class Document
 {
-    private String title;                       // TODO not used anywhere?
+    private String title;
     private Stack<String> undoStack;
     private Stack<String> redoStack;
 
+    /**
+     * Creates a document with the specified title, as well as a new undo stack and redo stack.
+     * @param title The title of the document.
+     */
     public Document(String title)
     {
         this.title = title;
@@ -15,35 +22,64 @@ public class Document
         redoStack = new Stack<String>();
     }
 
+    /**
+     * Adds the specified change to the document's undo stack.
+     * @param change The change to be made.
+     * @return A string confirming that the change is being made.
+     */
     public String makeChange(String change)
     {
         undoStack.push(change);
         return "Making Change: " + change;
     }
+    /**
+     * Pops the topmost change off of the undo stack and adds it to the redo stack.
+     * @return A string confirming that the change is being undone.
+     */
     public String undoChange()
     {
         String undone = undoStack.pop();
         redoStack.push(undone);
         return "Undoing: " + undone;
     }
+    /**
+     * Checks to see if there are any changes to undo.
+     * @return Whether or not the undo stack has anything in it.
+     */
     public boolean canUndo()
     {
         return undoStack.size() > 0;
     }
+    /**
+     * Pops the topmost change off of the redo stack and adds it to the undo stack.
+     * @return A string confirming that the change is being redone.
+     */
     public String redoChange()
     {
         String redone = redoStack.pop();
         undoStack.push(redone);
         return "Redoing: " + redone;
     }
+    /**
+     * Checks to see if there are any changes to redo.
+     * @return Whether or not the redo stack has anything in it.
+     */
     public boolean canRedo()
     {
         return redoStack.size() > 0;
     }
+    /**
+     * Creates a new StackIterator to iterate over the undo stack.
+     * @return The created iterator for the undo stack.
+     */
     public StackIterator getUndoIterator()
     {
         return new StackIterator(undoStack);
     }
+    /**
+     * Creates a new StackIterator to iterate over the redo stack.
+     * @return The created iterator for the redo stack.
+     */
     public StackIterator getRedoIterator()
     {
         return new StackIterator(redoStack);
--- iterator/StackIterator.java
+++ iterator/StackIterator.java
@@ -3,23 +3,39 @@ package iterator;
 import java.util.Iterator;
 import java.util.Stack;
 
+/**
+ * An object that iterates over a stack.
+ * @author Michael Pikula
+ */
 public class StackIterator implements Iterator<String>
 {
     private Stack<String> items;
     private int position;
 
+    /**
+     * Creates an iterator for the specified stack.
+     * @param items The stack to be iterated over.
+     */
     public StackIterator(Stack<String> items)
     {
         this.items = items;
         position = 0;
     }
 
+    /**
+     * Retrieves the next item in the stack.
+     * @return The item at the current position in the stack.
+     */
     public String next()
     {
         if (!hasNext())
             return null;
         return items.get(position++);   // Return item at index position, then increment position.
     }
+    /**
+     * Checks to see if the current position is past the end of the stack.
+     * @return Whether or not the stack has anything left to use.
+     */
     public boolean hasNext()
     {
         return position < items.size();
